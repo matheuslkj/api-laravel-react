86d1b50f2386b8b78ca6eec70634e451de4b92f9
matheuslkj <matheus050319@gmail.com>
2024-05-31 04:00:14 -0300

    Fazendo o GET para developers

--- frontend/frontendreact/src/App.js
+++ frontend/frontendreact/src/App.js
@@ -1,29 +1,31 @@
+import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter, Routes, Link, Route} from 'react-router-dom';
+import {Nav} from 'react-bootstrap';
 import Home from './components/Home';
 import Devs from './components/Devs';
 import Levels from './components/Levels';
-import {Nav} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-      <Nav variant='tabs'>
-        <Nav.Link as={Link} to="/" >Home</Nav.Link>
-        <Nav.Link as={Link} to="/devs" >Desenvolvedores</Nav.Link>
-        <Nav.Link as={Link} to="/niveis" >Niveis</Nav.Link>
-      </Nav>
+    // Defina o estado para armazenar os níveis
 
-      <Routes>
-        <Route path='/' index element={<Home/>}></Route>
-        <Route path='/devs' element={<Devs/>}></Route>
-        <Route path='/niveis' element={<Levels/>}></Route>
-      </Routes>
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Nav variant='tabs'>
+                    <Nav.Link as={Link} to="/" >Home</Nav.Link>
+                    <Nav.Link as={Link} to="/devs" >Desenvolvedores</Nav.Link>
+                    <Nav.Link as={Link} to="/niveis" >Níveis</Nav.Link>
+                </Nav>
 
-      </BrowserRouter>
-    </div>
-  );
+                <Routes>
+                    <Route path='/' index element={<Home/>}></Route>
+                    <Route path='/devs' element={<Devs/>}></Route>
+                    <Route path='/niveis' element={<Levels/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
